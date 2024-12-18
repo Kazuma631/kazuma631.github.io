e50f6143f538fc00b0551bdd79b7a13995c326e4
Kazuma631 <k39159219@gmail.com>
2024-12-18 08:35:09 +0000

    fixed script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const arrVerses = ["A new commandment I give to you, that you love one another: just as I have loved you, you also are to love one another. (John 13:34)", "Anyone who does not love does not know God, because God is love. (1 John 4:8)"];
 function nextVerse() {
   let randomNum = Math.floor(Math.random() * 2);
-  document.getElementById("verse").innerHTML(arrVerses[randomNum]);
+  document.getElementById("verse").innerHTML = arrVerses[randomNum];
 }
